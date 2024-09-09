c6bff1faf61547c4c6cd4ff4720f9f8a17817b03
Stepan Tytseiko <stepantytseyko@gmail.com>
2024-09-09 23:03:45 +0300

    Base react application

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return ( <div className='footerComponent bg-dark'>
+        {/* <h1>Footer</h1> */}
+        <div>© 2020 CyberTech LLC - Call ‪(331) 642-1137‬</div>
+    </div> );
+}
+
+export default Footer;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Main() {
+    return ( <div>
+        <br/>
+        <h1>MainPage</h1>
+    </div> );
+}
+
+export default Main;
